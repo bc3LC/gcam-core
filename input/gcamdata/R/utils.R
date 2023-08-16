e95ec02bb3fbc762059d2ddd6c55ab503294158e
russellhz <horowitz.russell@gmail.com>
2023-08-16 10:27:13 +0200

    separate out fuels for industrial feedstocks
    not sure if coal and gas feedstocks are necessary since GCAM assumes emissions are not captured from them (why???)

--- input/gcamdata/R/utils.R
+++ input/gcamdata/R/utils.R
@@ -89,7 +89,8 @@ load_csv_files <- function(filenames, optionals, quiet = FALSE, dummy = NULL, ..
     # Read the file header and extract the column type info from it
     assert_that(file.exists(fqfn))
     header <- find_header(fqfn)
-    col_types <- gsub(" |,", "", extract_header_info(header, label = "Column types:", fqfn, required = TRUE))
+    col_types <- gsub(" ", "", extract_header_info(header, label = "Column types:", fqfn, required = TRUE))
+
     # Attempt the file read
     # Note `options(warn = 2)` forces all warnings to errors...
     op <- options(warn = 2)
