a4d8ac3bcaf50984c0e43dc74e9bda1dd6376c9e
russellhz <horowitz.russell@gmail.com>
2025-03-11 10:55:57 +0100

    streamline L306.MAC_rename

--- input/gcamdata/R/zzPOLICY_L306.MAC_rename.R
+++ input/gcamdata/R/zzPOLICY_L306.MAC_rename.R
@@ -14,17 +14,18 @@
 #' @importFrom dplyr bind_rows distinct filter if_else left_join mutate select
 #' @author RLH April 2023
 module_policy_L306.MAC_rename <- function(command, ...) {
+  MODULE_INPUTS <- c(FILE = "policy/A_MAC_rename",
+                     FILE = "common/GCAM_region_names",
+                     "L252.ResMAC_fos",
+                     "L252.MAC_higwp",
+                     "L252.AgMAC",
+                     "L252.MAC_an",
+                     "L252.ResMAC_fos_tc_average",
+                     "L252.ResMAC_fos_phaseInTime",
+                     "L252.MAC_higwp_tc_average",
+                     "L252.MAC_higwp_phaseInTime")
   if(command == driver.DECLARE_INPUTS) {
-    return(c(FILE = "policy/A_MAC_rename",
-             FILE = "common/GCAM_region_names",
-             "L252.ResMAC_fos",
-             "L252.MAC_higwp",
-             "L252.AgMAC",
-             "L252.MAC_an",
-             "L252.ResMAC_fos_tc_average",
-             "L252.ResMAC_fos_phaseInTime",
-             "L252.MAC_higwp_tc_average",
-             "L252.MAC_higwp_phaseInTime"))
+    return(MODULE_INPUTS)
   } else if(command == driver.DECLARE_OUTPUTS) {
     return(c("L306.ResMAC_fos",
              "L306.MAC_higwp",
@@ -39,9 +40,9 @@ module_policy_L306.MAC_rename <- function(command, ...) {
     all_data <- list(...)[[1]]
 
     # Load required inputs
-    GCAM_region_names <- get_data(all_data, "common/GCAM_region_names")
-    A_MAC_rename <- get_data(all_data, "policy/A_MAC_rename") %>%
-      mutate(xml = if_else(grepl(".xml", xml), xml, paste0(xml, ".xml")))
+    get_data_list(all_data, MODULE_INPUTS)
+
+    A_MAC_rename <- A_MAC_rename %>% mutate(xml = if_else(grepl(".xml", xml), xml, paste0(xml, ".xml")))
 
     if ("ALL" %in% A_MAC_rename$region){
       A_MAC_rename_global <- A_MAC_rename %>%
@@ -54,50 +55,29 @@ module_policy_L306.MAC_rename <- function(command, ...) {
         bind_rows(A_MAC_rename_global)
     }
 
+    # function to filter and rename non-co2
+    process_MAC_data <- function(data, rename_table = A_MAC_rename, sector_col) {
+      sector_col_sym <- rlang::ensym(sector_col)  # Convert column name to symbol
+      sector_col_str <- rlang::as_name(sector_col_sym)  # Convert symbol to string
+
+      data %>%
+        rename(supplysector = !!sector_col_sym) %>%  # Temporarily rename to "supplysector"
+        semi_join(rename_table, by = c("region", "supplysector", "Non.CO2")) %>%
+        left_join(rename_table, by = c("region", "supplysector", "Non.CO2")) %>%
+        mutate(market.name = new.market.name) %>%
+        select(-new.market.name) %>%
+        rename(!!sector_col_sym := supplysector)  # Rename back to original column name
+    }
+
     # For each MAC object, just need to filter it and rename Non-CO2
-    L306.ResMAC_fos <- get_data(all_data, "L252.ResMAC_fos")  %>%
-      semi_join(A_MAC_rename, by = c("region", "resource" = "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "resource" = "supplysector", "Non.CO2")) %>%
-      mutate(market.name = new.market.name) %>%
-      select(-new.market.name)
-
-    L306.MAC_higwp <- get_data(all_data, "L252.MAC_higwp")   %>%
-      semi_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      mutate(market.name = new.market.name) %>%
-      select(-new.market.name)
-
-    L306.AgMAC <- get_data(all_data, "L252.AgMAC")  %>%
-      semi_join(A_MAC_rename, by = c("region", "AgSupplySector" = "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "AgSupplySector" ="supplysector", "Non.CO2")) %>%
-      mutate(market.name = new.market.name) %>%
-      select(-new.market.name)
-
-    L306.MAC_an <- get_data(all_data, "L252.MAC_an")  %>%
-      semi_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      mutate(market.name = new.market.name) %>%
-      select(-new.market.name)
-
-    L306.ResMAC_fos_tc_average <- get_data(all_data, "L252.ResMAC_fos_tc_average")  %>%
-      semi_join(A_MAC_rename, by = c("region", "resource" = "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "resource" = "supplysector", "Non.CO2")) %>%
-      select(-new.market.name)
-
-    L306.ResMAC_fos_phaseInTime <- get_data(all_data, "L252.ResMAC_fos_phaseInTime") %>%
-      semi_join(A_MAC_rename, by = c("region", "resource" = "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "resource" = "supplysector", "Non.CO2")) %>%
-      select(-new.market.name)
-
-    L306.MAC_higwp_tc_average <- get_data(all_data, "L252.MAC_higwp_tc_average")   %>%
-      semi_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      select(-new.market.name)
-
-    L306.MAC_higwp_phaseInTime <- get_data(all_data, "L252.MAC_higwp_phaseInTime")  %>%
-      semi_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      left_join(A_MAC_rename, by = c("region", "supplysector", "Non.CO2")) %>%
-      select(-new.market.name)
+    L306.ResMAC_fos <- process_MAC_data(L252.ResMAC_fos, A_MAC_rename, resource)
+    L306.MAC_higwp <- process_MAC_data(L252.MAC_higwp, A_MAC_rename, supplysector)
+    L306.AgMAC <- process_MAC_data(L252.AgMAC, A_MAC_rename, AgSupplySector)
+    L306.MAC_an <- process_MAC_data(L252.MAC_an, A_MAC_rename, supplysector)
+    L306.ResMAC_fos_tc_average <- process_MAC_data(L252.ResMAC_fos_tc_average, A_MAC_rename, resource)
+    L306.ResMAC_fos_phaseInTime <- process_MAC_data(L252.ResMAC_fos_phaseInTime, A_MAC_rename, resource)
+    L306.MAC_higwp_tc_average <- process_MAC_data(L252.MAC_higwp_tc_average, A_MAC_rename, supplysector)
+    L306.MAC_higwp_phaseInTime <- process_MAC_data(L252.MAC_higwp_phaseInTime, A_MAC_rename, supplysector)
 
 
     # Produce outputs
